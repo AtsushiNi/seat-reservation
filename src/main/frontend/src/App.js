5eba1866bc72bf23174ebc176749e3b93a8aab57
AtsushiNi <atsushi9731@gmail.com>
2024-01-25 13:37:48 +0900

    react-leaflet

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ImageOverlay, MapContainer, useMapEvents } from "react-leaflet";
 
 function App() {
+  const Debugger = () => {
+    const map = useMapEvents({
+      click: (e) => {
+        console.log(e.latlng);
+      },
+    });
+    return null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header></header>
+      <main>
+        <MapContainer
+          center={[100,800]}
+          zoom={0}
+          maxZoom={1}
+          scrollWheelZoom={false}
         >
-          Learn React
-        </a>
-      </header>
+          <ImageOverlay
+            url="/seat.png"
+            bounds={[
+              [0,0],
+              [400,800],
+            ]}
+          />
+          <Debugger />
+        </MapContainer>
+      </main>
     </div>
   );
 }
